24f14437007bf9f99a2573b9aa6aa55f05a42375
dobrapyra <zielinski00@gmail.com>
2022-05-08 19:50:03 +0200

    replace helpers anonymous arrow functions by named

--- src/helpers.js
+++ src/helpers.js
@@ -3,14 +3,14 @@
  * @param {Array} arr array or other iterable collection
  * @param {Function} cb callback function for each array element
  */
-export const each = (arr, cb) => {
+export function each(arr, cb) {
   const l = arr.length;
   for (let i = 0; i < l; i++) {
     const result = cb(arr[i], i, arr);
     if (result === true) continue;
     if (result === false) break;
   }
-};
+}
 
 /**
  * lerp
@@ -19,20 +19,20 @@ export const each = (arr, cb) => {
  * @param {Number} factor in range 0.0 - 1.0
  * @returns interpolated value
  */
-export const lerp = (begin, end, factor) => {
+export function lerp(begin, end, factor) {
   return begin + ((end - begin) * factor);
-};
+}
 
 /**
  * style
  * @param {Element} el
  * @param {Object} styles
  */
-export const style = (el, styles) => {
+export function style(el, styles) {
   Object.keys(styles).forEach(styleKey => {
     el.style[styleKey] = styles[styleKey];
   });
-};
+}
 
 export default {
   each,
