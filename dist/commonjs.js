46dafefa9259d07479a6353313d4bf939b196b4e
dobrapyra <zielinski00@gmail.com>
2023-03-12 17:50:09 +0100

    update build & docs

--- dist/commonjs.js
+++ dist/commonjs.js
@@ -1,3 +1,13 @@
+function $parcel$defineInteropFlag(a) {
+  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
+}
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$defineInteropFlag(module.exports);
+
+$parcel$export(module.exports, "default", () => $2776a60caf88deef$export$2e2bcd8739ae039);
 /**
  * lerp
  * @param {Array} arr array or other iterable collection
