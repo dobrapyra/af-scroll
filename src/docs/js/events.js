d20b5170a17e2a6b951b4d72773dec0dafd3052a
dobrapyra <zielinski00@gmail.com>
2023-03-12 16:06:08 +0100

    update docs

---
+++ src/docs/js/events.js
@@ -0,0 +1,20 @@
+import createAFScroll from '../../../dist/esmodule';
+
+(function(){
+  // AFScroll
+  const afScroll = createAFScroll({
+    onUpdate: (scroll) => { console.log('update', scroll); },
+    onComplete: (scroll) => { console.log('complete', scroll); },
+  });
+
+  [
+    { selector: '[data-scroll-top]', cb: () => { afScroll.scrollTo(0); } },
+    { selector: '[data-destroy]', cb: () => { afScroll.destroy(); } },
+    { selector: '[data-reinit]', cb: () => { afScroll.init(); } },
+    { selector: '[data-lock]', cb: () => { afScroll.lock(); } },
+    { selector: '[data-unlock]', cb: () => { afScroll.unlock(); } },
+  ].map(({ selector, cb }) => {
+    const button = document.querySelector(selector);
+    button.addEventListener('click', cb);
+  })
+})();
---
+++ src/docs/js/smooth.js
@@ -0,0 +1,19 @@
+import createAFScroll from '../../../dist/esmodule';
+
+(function(){
+  // AFScroll
+  const afScroll = createAFScroll({
+    smoothForce: 0.95,
+  });
+
+  [
+    { selector: '[data-scroll-top]', cb: () => { afScroll.scrollTo(0); } },
+    { selector: '[data-destroy]', cb: () => { afScroll.destroy(); } },
+    { selector: '[data-reinit]', cb: () => { afScroll.init(); } },
+    { selector: '[data-lock]', cb: () => { afScroll.lock(); } },
+    { selector: '[data-unlock]', cb: () => { afScroll.unlock(); } },
+  ].map(({ selector, cb }) => {
+    const button = document.querySelector(selector);
+    button.addEventListener('click', cb);
+  })
+})();
